aaa6aad4a4f5b77831372a4cd881c05605ad53ef
Erik <erikhny@gmail.com>
2025-11-17 20:17:17 +0100

    fix: minor authorize progress

--- src/oauth/login.rs
+++ src/oauth/login.rs
@@ -14,11 +14,30 @@ pub struct AuthorizeQueryParams {
 
 pub async fn authorize(State(ctx): State<ApiContext>, Query(params): Query<AuthorizeQueryParams>) -> Redirect {
     println!("{:?}", params);
+
     let client_id = params.client_id.as_str();
-    let client = ctx.client_store.get(client_id);
-    if let Some(client) = client {
-        let cid = &client.client_id;
-       return Redirect::to(format!("/test/{cid}").as_str());
+    let client = match ctx.client_store.get(client_id) {
+        Some(client) => client,
+        None => return Redirect::permanent("/"), // TODO: Properly handle error here.
+    };
+
+    let redirect_uri = match params.redirect_uri {
+        Some(redirect_uri) => redirect_uri,
+        None => return Redirect::permanent("/"), // TODO: Properly handle error here.
+    };
+    if !&client.redirect_uris.contains(&redirect_uri) {
+        return Redirect::permanent("/"); // TODO: Properly handle error here.
     }
+
+    if client.require_pkce {
+        // TODO: Handle pkce here.
+    }
+
+    // Validate scopes?
+
+    // If all validation succeeds:
+    // Make decision to redirect to actual login or SSO handling?
+
+
     Redirect::to("/test/authorize")
 }
